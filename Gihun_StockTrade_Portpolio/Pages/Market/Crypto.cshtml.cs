2edf9ed13ebbb79bd901a2e8404e57b38ecfbcc6
feverishkk <feverishkk@gmail.com>
2023-08-15 16:46:31 +0900

    Add Crypto Page

--- Gihun_StockTrade_Portpolio/Pages/Market/Crypto.cshtml.cs
+++ Gihun_StockTrade_Portpolio/Pages/Market/Crypto.cshtml.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using StockTrade.Application.Interfaces.API;
+using StockTrade.Application.ViewModel.API.MarketViewModel.Crypto;
 
 namespace Gihun_StockTrade_Portpolio.Pages.Market
 {
     public class CryptoModel : PageModel
     {
-        public void OnGet()
+        private readonly IMarketAPIRepository _marketAPIRepo;
+        public List<Quote> CryptoResult { get; set; }
+
+        public CryptoModel( IMarketAPIRepository marketAPIRepo )
         {
+            _marketAPIRepo = marketAPIRepo; 
         }
+
+        public async Task OnGet()
+        {
+            await OnGetCrypto();
+        }
+
+        public async Task OnGetCrypto()
+        {
+            var result = await _marketAPIRepo.GetCrypto();
+
+            if ( result == null )
+            {
+                return;
+            }
+
+            CryptoResult = result.finance.result[0].quotes;
+        }
+
+
+
     }
 }
